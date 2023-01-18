cf764fb7968d24dcbabb97cc9fe396f1b0fd0ae7
mathiskakal <114522530+mathiskakal@users.noreply.github.com>
2023-01-18 23:00:39 +0100

    add find image from path func

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -1,7 +1,9 @@
 
 // importing the module 
 mod args;
+use std::{io::BufReader, fs::File};
 use args::Args;
+use image::{io::Reader, ImageFormat, DynamicImage};
 
 /*
 Rather than doing this to instanciate a new arg, we can create a method that'll do it for us
@@ -19,20 +21,14 @@ fn main() {
 
 fn main() {
   let args = Args::new();
-  println!("{:?}", args);
+  let (image_1, image_format_1) = find_image_from_path(args.image_1);
+  let (image_2, image_format_2) = find_image_from_path(args.image_2);
+  // println!("{:?}", args);
 }
 
-
-
-/*
-The new()method on String kinda looks like this:
-
-impl String {
-  fn new() -> Self {
-    String {
-      vec: Vec::new(),
-    }
-  }
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+  let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+  let image_format: ImageFormat = image_reader.format().unwrap();
+  let image: DynamicImage= image_reader.decode().unwrap();
+  (image, image_format)
 }
-
-*/
