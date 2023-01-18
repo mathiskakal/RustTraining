189a7e2396433d831389c06f9979a47983804853
mathiskakal <114522530+mathiskakal@users.noreply.github.com>
2023-01-18 23:18:40 +0100

    add dimension comparison

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -3,7 +3,7 @@
 mod args;
 use std::{io::BufReader, fs::File};
 use args::Args;
-use image::{io::Reader, ImageFormat, DynamicImage};
+use image::{io::Reader, ImageFormat, DynamicImage, GenericImageView, imageops::FilterType::Triangle};
 
 /*
 Rather than doing this to instanciate a new arg, we can create a method that'll do it for us
@@ -17,13 +17,22 @@ fn main() {
 }
 */
 
+enum ImageDataErrors {
+    DifferentImageFormats,
+}
 
-
-fn main() {
+fn main() -> Result<(), ImageDataErrors> { // Handling errors
   let args = Args::new();
   let (image_1, image_format_1) = find_image_from_path(args.image_1);
   let (image_2, image_format_2) = find_image_from_path(args.image_2);
   // println!("{:?}", args);
+
+
+  if image_1_format != image_format_2 {
+    return Err(ImageDataErrors::DifferentImageFormats);
+  }
+
+  Ok(())
 }
 
 fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
@@ -32,3 +41,10 @@ fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
   let image: DynamicImage= image_reader.decode().unwrap();
   (image, image_format)
 }
+
+fn get_smallest_dimension(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
+  let pix_1 = dim_1.0 * dim_1.1;
+  let pix_2 = dim_2.0 * dim_2.1;
+  return if pix_1 < pix_2 { dim_1 } else { dim_2 };
+}
+
