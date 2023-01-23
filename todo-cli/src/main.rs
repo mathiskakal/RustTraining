1f9c3c3add5b7dde267ebd5efa635979f4afccab
mathiskakal <mathis.kakal@algosup.com>
2023-01-23 11:09:31 +0100

    create struct and todo method

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,7 +1,27 @@
+// save some time typing
+use std::collections::HashMap
+
+
+
+// Main function
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
 
     println!("{:?},{:?}", action, item);
-    
+
+}
+
+// Definitions
+struct Todo {
+    // use rust buult in hashmap to store k/v pairs
+    map: HashMap<String, bool>,
+}
+
+impl Todo {
+    fn insert (&mut self, key: String) {
+        //insert a new item into map
+        //pass true as a value
+        self.map.insert(k, true);
+    }
 }
