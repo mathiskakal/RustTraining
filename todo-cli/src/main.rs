315ee61bbaaf7e3387951153bfbe07108065284a
mathiskakal <mathis.kakal@algosup.com>
2023-01-23 11:28:08 +0100

    implement save to file method

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -22,6 +22,15 @@ impl Todo {
     fn insert (&mut self, key: String) {
         //insert a new item into map
         //pass true as a value
-        self.map.insert(k, true);
+        self.map.insert(key, true);
+    }
+    // simple method to store on disk
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+        std::fs::write("db.txt", content)
     }
 }
