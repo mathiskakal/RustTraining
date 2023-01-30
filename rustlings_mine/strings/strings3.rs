7e325f0b009185d352a146ff572bfa949bca2b4e
mathiskakal <mathis.kakal@algosup.com>
2023-01-30 14:07:00 +0100

    finish strings3

--- rustlings_mine/strings/strings3.rs
+++ rustlings_mine/strings/strings3.rs
@@ -1,21 +1,26 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut s = String::from(input);
+    let trim = s.trim();
+    trim.to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut s = String::from(input);
+    s.push_str(" world!");
+    s
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+
+    let mut s = String::from(input);
+    let repl = s.replace("cars", "balloons");
+    repl
 }
 
 #[cfg(test)]
